f715cfe7ea4862c0d5bb47bb4c17294506c78ec4
Kenichi Saito <hajime@studiohff.net>
2024-12-08 16:07:35 +0900

    Chapter 2.5

--- main.tf
+++ main.tf
@@ -2,12 +2,18 @@ provider "aws" {
   region = "us-east-2"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
 resource "aws_security_group" "instance" {
   name = "terraform-exaple-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -21,7 +27,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   user_data_replace_on_change = true
@@ -29,4 +35,9 @@ resource "aws_instance" "example" {
   tags = {
     Name = "terraform-example"
   }
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
